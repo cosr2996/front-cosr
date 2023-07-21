64c26dc09541e08c3bf1313fe981a351f291ac02
cosr2996 <115452805+cosr2996@users.noreply.github.com>
2023-07-21 14:20:54 -0600

    solucionando errores2

--- src/components/ItemSkill.jsx
+++ src/components/ItemSkill.jsx
@@ -1,8 +1,23 @@
+import { Progress } from 'flowbite-react';
 
-
-const ItemSkill = () => {
+const ItemSkill = ({title,image,progress,nivel}) => {
   return (
-    <div>ItemSkill</div>
+    <div className=" bg-white w-60 h-60 flex  flex-col justify-center items-center rounded-md">
+    {/* logo */}
+    <div className="w-1/2">
+        <img className='w-full'  src={image} alt="" />
+    </div>
+    <h2 className="font-bold">{title}</h2>
+    <div className="w-3/4 		">
+    <Progress
+         progress={progress}
+         color="dark"
+         size="xl"
+         />
+    </div>
+    <span className="opacity-50">{nivel}</span>
+
+  </div>
   )
 }
 
