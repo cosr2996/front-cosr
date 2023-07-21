2bf4a5a18326a2c07803ad28f20c05e29be15b43
cosr2996 <115452805+cosr2996@users.noreply.github.com>
2023-07-21 14:22:20 -0600

    solucionando errores2

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -18,18 +18,18 @@ const Skills = () => {
     <>
       <h1 className="text-white text-center text-6xl font-bold animate-fade-in-y">Skills</h1>
       <div className="p-10 flex gap-5 justify-around flex-wrap">
-        {/* <Skill title="React" image={react} progress={50} nivel={"Medio"} />
-        <Skill title="Node" image={node} progress={90} nivel={"Alto"} />
-        <Skill image={html} title="HTML" progress={90} nivel={"Alto"} />
-        <Skill image={css} title="CSS" progress={90} nivel={"Alto"} />
-        <Skill image={tailwind} title="TailWind" progress={90} nivel={"Alto"} />
-        <Skill image={js} title="Javascript" progress={50} nivel={"Medio"} />
-        <Skill image={mongo} title="MongoDB" progress={50} nivel={"Medio"} />
-        <Skill image={express} title="Express" progress={50} nivel={"Medio"} />
-        <Skill image={next} title="Next" progress={25} nivel={"Bajo"} />
-        <Skill image={vitejs} title="Vite" progress={90} nivel={"Alto"} />
-        <Skill image={redux} title="Redux" progress={25} nivel={"Bajo"} />
-        <Skill image={prisma} title="Prisma" progress={25} nivel={"Bajo"} /> */}
+        <ItemSkill title="React" image={react} progress={50} nivel={"Medio"} />
+        <ItemSkill title="Node" image={node} progress={90} nivel={"Alto"} />
+        <ItemSkill image={html} title="HTML" progress={90} nivel={"Alto"} />
+        <ItemSkill image={css} title="CSS" progress={90} nivel={"Alto"} />
+        <ItemSkill image={tailwind} title="TailWind" progress={90} nivel={"Alto"} />
+        <ItemSkill image={js} title="Javascript" progress={50} nivel={"Medio"} />
+        <ItemSkill image={mongo} title="MongoDB" progress={50} nivel={"Medio"} />
+        <ItemSkill image={express} title="Express" progress={50} nivel={"Medio"} />
+        <ItemSkill image={next} title="Next" progress={25} nivel={"Bajo"} />
+        <ItemSkill image={vitejs} title="Vite" progress={90} nivel={"Alto"} />
+        <ItemSkill image={redux} title="Redux" progress={25} nivel={"Bajo"} />
+        <ItemSkill image={prisma} title="Prisma" progress={25} nivel={"Bajo"} /> 
       </div>
     </>
   );
