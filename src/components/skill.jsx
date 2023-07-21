4837a6a7b0521a9c4f14b7b3e8b502ad45e3ce88
cosr2996 <115452805+cosr2996@users.noreply.github.com>
2023-07-21 14:14:34 -0600

    solucionando errores

--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -1,26 +1,9 @@
-import { Progress } from 'flowbite-react';
+import React from 'react'
 
-const Skill = ({title,image,progress,nivel}) => {
+const Skill = () => {
   return (
-    
-  <div className=" bg-white w-60 h-60 flex  flex-col justify-center items-center rounded-md">
-    {/* logo */}
-    <div className="w-1/2">
-        <img className='w-full'  src={image} alt="" />
-    </div>
-    <h2 className="font-bold">{title}</h2>
-    <div className="w-3/4 		">
-    <Progress
-         progress={progress}
-         color="dark"
-         size="xl"
-         />
-    </div>
-    <span className="opacity-50">{nivel}</span>
+    <div>Skill</div>
+  )
+}
 
-  </div>
-
-)
-};
-
-export default Skill;
+export default Skill
